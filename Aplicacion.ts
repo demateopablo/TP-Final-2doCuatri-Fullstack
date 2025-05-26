b227fffd85511ffca8f15be6d9a43231c05f5f51
Pablo Demateo <pablodem32@gmail.com>
2025-05-26 19:24:58 -0300

    Update Aplicacion.ts

--- Aplicacion.ts
+++ Aplicacion.ts
@@ -1,16 +1,62 @@
-import * as rs from 'readline-sync';
-import { Jugador } from './Jugador';
-import { Casino } from './Casino';
+import * as rdl from 'readline-sync';
+import { Casino } from "./Casino";
+import { Jugador } from "./Jugador";
 
-export class Aplicacion {
+export class Aplicacion{
 
-    public static instancia: Aplicacion;
-    public casino: Casino;
-    public jugador: Jugador;
+  public static instancia: Aplicacion; // patron de diseño Singleton
+  private casino: Casino;
+  private jugador: Jugador;
 
-    constructor(pCasino: Casino, pJugador: Jugador) {
-        this.casino = pCasino;
-        this.jugador = pJugador;
+  constructor(casino: Casino, jugador: Jugador){
+    this.casino = casino;
+    this.jugador = jugador;
+  }
+
+  // registrarJugador(jugador: Jugador){
+  //   try {
+  //     if (jugador.getEdad() > this.casino.getEdadMin()) {
+  //       this.jugadores.push(jugador);
+  //     }
+  //   } catch (error) {
+  //     let e: Error = new Error("\nNo tienes edad suficiente para ingresar al casino\n")
+  //     console.log(e.message);
+  //   }
+  // }
+
+  // patron de diseño Singleton
+  public static getInstancia(casino: Casino, jugador: Jugador): Aplicacion{
+    if(!this.instancia){
+        this.instancia = new Aplicacion(casino, jugador);
+    }else{
+        console.log("la instancia ya existe");
     }
+    return this.instancia;
+  }
 
+  getJugador(): void{
+    console.log(`\n${this.jugador.toString()}`);
+  }
+
+  mostrarMenu():void{
+    console.log(`Bienvenido a Casino ${this.casino.getNombre()}`);
+    this.casino.listarJuegos();
+    console.log("0 Salir");
+    let opcion: number = this.preguntar("Elija una opcion: ",this.casino.getCantJuegos());
+    if (opcion == 0) {
+      console.log("Funcion salir y exportar saldo");
+    }else{
+      this.casino.getJuego(opcion-1).jugar(this.jugador);
+    }
+    this.mostrarMenu();
+  }
+
+  preguntar(mensaje: string, cantOpciones: number): number{
+    let opcElegida: number;
+    do{
+    opcElegida= rdl.questionInt(mensaje);
+    } while (opcElegida < 0 || opcElegida > cantOpciones)
+    return opcElegida;
+  }
+  
 }
