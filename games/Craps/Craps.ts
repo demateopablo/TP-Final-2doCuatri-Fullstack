9e391a530fdf2e1cafb743201590bd2712651a2b
Gustavo Arias <ariasgustavo3a@gmail.com>
2025-05-30 08:07:56 -0300

    modularizando Craps

--- games/Craps/Craps.ts
+++ games/Craps/Craps.ts
@@ -12,10 +12,24 @@ export class Craps extends Juego{
     this.dado = new Dado(6);
   }
 
-  verSiEsPase(sumaDados: number): string{
-    if (sumaDados === 7 || sumaDados === 11) return "gana";
-    if (sumaDados === 2 || sumaDados === 3 || sumaDados === 12) return "pierde";
-    return "punto";
+  // verSiEsPase(sumaDados: number): string{
+  //   if (sumaDados === 7 || sumaDados === 11) return "gana";
+  //   if (sumaDados === 2 || sumaDados === 3 || sumaDados === 12) return "pierde";
+  //   return "punto";
+  // }
+
+  verSiEsWin(sumaDados: number): boolean{
+    if (sumaDados === 7 || sumaDados === 11){
+      return true;
+    }
+    return false;
+  }
+
+  verSiEsCrap(sumaDados: number): boolean{
+    if (sumaDados === 2 || sumaDados === 3 || sumaDados === 12){
+      return true;
+    }
+    return false;
   }
 
   verSiEsPunto(sumaDados: number, punto: number): string {
@@ -32,44 +46,61 @@ export class Craps extends Juego{
     return dado1+dado2;
   }
 
+  logicaScraps(jugador: Jugador, apuesta: number){
+    let punto: number | null = null;
+    let opcApuestaInicial: number = rdl.questionInt("\nSeleccion con qué modo de juego quiere iniciar la partida\n\t1 - Pass Line\n\t2 - Don't Pass Bar");
+    let sumaDados: number = this.tirarDosDados();
+    if (opcApuestaInicial === 1){
+      if (!this.verSiEsCrap(sumaDados) && !this.verSiEsWin(sumaDados)){
+        punto = sumaDados;
+        console.log(`El punto es ${punto}`);
+      } else if(this.verSiEsWin(sumaDados)){
+        console.log(`La tirada inicial es ${sumaDados}.  GANA!!!`);
+        this.pagar(apuesta, jugador);
+      } else {
+        console.log(`La tirada inicial es ${sumaDados}.  PIERDE!!!`);
+      }
+      this.jugar(jugador);
+    }
+    if (opcApuestaInicial === 2){
+      if (!this.verSiEsCrap(sumaDados) && !this.verSiEsWin(sumaDados)){
+        punto = sumaDados;
+        console.log(`El punto es ${punto}`);
+      } else if(this.verSiEsWin(sumaDados)){
+        console.log(`La tirada inicial es ${sumaDados}.  GANA!!!`);
+        this.pagar(apuesta, jugador);
+      } else {
+        console.log(`La tirada inicial es ${sumaDados}.  PIERDE!!!`);
+      }
+      this.jugar(jugador);
+    }
+    let contador: number = 0;
+    while (true) {
+      contador++;
+      sumaDados = this.tirarDosDados();
+      console.log(`Tirada ${contador} sale ${sumaDados}`);
+      resultado = this.verSiEsPunto(sumaDados, punto!);
+      if (resultado === "gana") {
+        console.log(`\n-----------------\nEl punto ${punto} sale. Gana!!!\n-----------------`);
+        this.pagar(apuesta, jugador);
+        console.log(jugador.toString());
+        break;
+      }
+      if (resultado === "pierde") {
+        console.log(`\n-----------------\nSale un 7. Pierde.\n-----------------`);
+        console.log(jugador.toString());
+        break;
+      }
+    }
+  }
+
   jugar(jugador:Jugador): void {
     console.log(`Hola ${jugador.getNombre()}, estas por jugar ${this.nombre} `);
     let apuesta: number = rdl.questionInt("\nCuanto dinero deseas apostar?: $")
     if(super.jugadorApto(jugador.getMonedero(),apuesta)){
         if(apuesta >= this.apuestaMin){
           jugador.modificarSaldo((-1)*apuesta);
-          let punto: number | null = null;
-          let sumaDados: number = this.tirarDosDados();
-          let resultado: string = this.verSiEsPase(sumaDados);
-          if (resultado === "punto") {
-            punto = sumaDados;
-            console.log(`El punto es ${punto}`);
-          }else {
-            console.log(`La tirada inicial es ${sumaDados}.  ${resultado}`);
-            if (resultado === "gana") {
-              this.pagar(apuesta, jugador);
-            }
-            console.log(`${jugador.toString()}\n`);
-            return;
-          }
-          let contador: number = 0;
-          while (true) {
-            contador++;
-            sumaDados = this.tirarDosDados();
-            console.log(`Tirada ${contador} sale ${sumaDados}`);
-            resultado = this.verSiEsPunto(sumaDados, punto!);
-            if (resultado === "gana") {
-              console.log(`\n-----------------\nEl punto ${punto} sale. Gana!!!\n-----------------`);
-              this.pagar(apuesta, jugador);
-              console.log(jugador.toString());
-              break;
-            }
-            if (resultado === "pierde") {
-              console.log(`\n-----------------\nSale un 7. Pierde.\n-----------------`);
-              console.log(jugador.toString());
-              break;
-            }
-          }
+          
       }else{
         console.log(`La apuesta que deseas hacer no supera la apuesta minima para este juego, la apuesta minima es de $${this.apuestaMin}\n`);
         jugador.apostar(this);
